ba70b1784ce8b2db03637ab2d96f1696a37369c9
Thomas Lademann <thomas@asphaltschneider.ch>
2020-12-28 00:58:23 +0100

    better working version with delays between cam switches

--- irspeccamswitcher.py
+++ irspeccamswitcher.py
@@ -3,16 +3,26 @@ import time
 from datetime import datetime
 import configparser
 
-# GroupName | GroupNum
-# Nose | 1          Interesting
-# Gearbox | 2       Interesting
-# Roll Bar | 3
-# LF Susp | 4
-# LR Susp | 5
-# Gyro | 6          Interesting
-# RF Susp | 7
-# RR Susp | 8
-# Cockpit |
+# 20-12-27 23:42:04 Name: Nose GroupNum 1
+# 20-12-27 23:42:04 Name: Gearbox GroupNum 2
+# 20-12-27 23:42:04 Name: Roll Bar GroupNum 3
+# 20-12-27 23:42:04 Name: LF Susp GroupNum 4
+# 20-12-27 23:42:04 Name: LR Susp GroupNum 5
+# 20-12-27 23:42:04 Name: Gyro GroupNum 6
+# 20-12-27 23:42:04 Name: RF Susp GroupNum 7
+# 20-12-27 23:42:04 Name: RR Susp GroupNum 8
+# 20-12-27 23:42:04 Name: Cockpit GroupNum 9
+# 20-12-27 23:42:04 Name: TV1 GroupNum 10
+# 20-12-27 23:42:04 Name: TV2 GroupNum 11
+# 20-12-27 23:42:04 Name: TV3 GroupNum 12
+# 20-12-27 23:42:04 Name: Scenic GroupNum 13
+# 20-12-27 23:42:04 Name: Pit Lane GroupNum 14
+# 20-12-27 23:42:04 Name: Pit Lane 2 GroupNum 15
+# 20-12-27 23:42:04 Name: Chopper GroupNum 16
+# 20-12-27 23:42:04 Name: Blimp GroupNum 17
+# 20-12-27 23:42:04 Name: Chase GroupNum 18
+# 20-12-27 23:42:04 Name: Far Chase GroupNum 19
+# 20-12-27 23:42:04 Name: Rear Chase GroupNum 20
 
 VERSION = "0.01"
 configfile = "irspeccamswitcher.cfg"
@@ -51,34 +61,16 @@ def check_iracing():
             print(curtimestamp, "Start type is", standingstart, "start")
 
 
-def loop():
+def loop(carNum, last_epoch):
 
     ir.freeze_var_buffer_latest()
-    secondcar = 32
     switch_cam = 0
+    epoch_time = int(time.time())
     if ir["DriverInfo"]:
         spec_on = ir["CamCarIdx"]
-        #print("cam is on", spec_on)
-        # print(secondcar, ir["CarIdxEstTime"][secondcar])
-        # print(spec_on, ir["CarIdxEstTime"][spec_on])
-        # print("estimated", ir["DriverInfo"]["DriverCarEstLapTime"])
-        # print("half minus=", (-0.5 * ir["DriverInfo"]["DriverCarEstLapTime"]))
-        # print("half plus =", (0.5 * ir["DriverInfo"]["DriverCarEstLapTime"]))
-        # reltime = ir["CarIdxEstTime"][secondcar] - ir["CarIdxEstTime"][spec_on]
-        #
-        # print("initial reltime", reltime)
-        # while reltime < (-0.5 * ir["DriverInfo"]["DriverCarEstLapTime"]):
-        #     reltime += ir["DriverInfo"]["DriverCarEstLapTime"]
-        # while reltime > (0.5 * ir["DriverInfo"]["DriverCarEstLapTime"]):
-        #     reltime -= ir["DriverInfo"]["DriverCarEstLapTime"]
-        # while reltime < 0:
-        #     reltime += ir["DriverInfo"]["DriverCarEstLapTime"]
-        # # if i > len(ir["DriverInfo"]["Drivers"]) / 2:
-        # while reltime > 0:
-        #     reltime -= ir["DriverInfo"]["DriverCarEstLapTime"]
-        # print(reltime)
         pctspecon = ir["CarIdxLapDistPct"][spec_on] * 100
 
+        driversindistance = 0
         for i in range(len(ir["CarIdxLapDistPct"])):
             if i != spec_on:
                 if ir["CarIdxLapDistPct"][i] != -1:
@@ -86,38 +78,44 @@ def loop():
                     #if pctdriver < 0:
                     #    pctdriver += 100
                     #print("   ", i, pctdriver)
-                    if pctdriver < 1 and pctdriver > -0.2:
+                    if pctdriver < 0.5 and pctdriver > -0.2:
+                        driversindistance += 1
                         switch_cam = 1
-                    elif pctdriver > -1 and pctdriver <= -0.2:
+                    elif pctdriver > -0.5 and pctdriver <= -0.2:
+                        driversindistance += 1
                         switch_cam = 2
 
-            #elif i == spec_on:
-            #    print("+++", i, 0)
+        if epoch_time - last_epoch > 3:
+            if switch_cam == 1 and driversindistance == 1:
+                #print("1 Switch Cam to", carNum)
+                ir.cam_switch_num(carNum, 19, 0)
+            elif switch_cam == 2 and driversindistance == 1:
+                #print("2 Switch Cam to", carNum)
+                ir.cam_switch_num(carNum, 2, 0)
+            elif switch_cam > 0 and driversindistance > 1:
+                ir.cam_switch_num(carNum, 16, 0)
+            else:
+                #print("e Switch Cam to", carNum)
+                ir.cam_switch_num(carNum, 10, 0)
+            return epoch_time
 
-        if switch_cam == 1:
-            ir.cam_switch_num(2, 19, 0)
-        elif switch_cam == 2:
-            ir.cam_switch_num(2, 2, 0)
-        else:
-            ir.cam_switch_num(2, 10, 0)
-
-
-    #print("-------------------------------------------------------------")
-    #time.sleep(1)
-
+    return last_epoch
 
 
 def findDriver(uid):
     if ir["DriverInfo"]:
         for i in range(len(ir["DriverInfo"]["Drivers"])):
             if ir["DriverInfo"]["Drivers"][i]["UserID"] == int(uid):
-                #print("+++ Number", ir["DriverInfo"]["Drivers"][i]["CarNumber"], "UID",
-                #      ir["DriverInfo"]["Drivers"][i]["UserID"], "==", uid, ", Driver", ir["DriverInfo"]["Drivers"][i]["UserName"])
+                print("+++ Number", ir["DriverInfo"]["Drivers"][i]["CarNumber"], "UID",
+                      ir["DriverInfo"]["Drivers"][i]["UserID"], "==", uid, ", Driver", ir["DriverInfo"]["Drivers"][i]["UserName"])
+                ir.cam_switch_num(ir["DriverInfo"]["Drivers"][i]["CarNumber"], 10, 0)
                 return ir["DriverInfo"]["Drivers"][i]["CarNumber"]
-            else:
+            #else:
                 #print("    Number", ir["DriverInfo"]["Drivers"][i]["CarNumber"], "UID", ir["DriverInfo"]["Drivers"][i]["UserID"], "!=", uid, ", Driver", ir["DriverInfo"]["Drivers"][i]["UserName"])
-                ir.cam_switch_num(2,10,0)
-    return "none"
+
+    print("Driver was not found!")
+    return -1
+
 
 def cameras():
     # datetime object containing current date and time
@@ -129,7 +127,6 @@ def cameras():
         cameras = ir["CameraInfo"]["Groups"]
         for i in cameras:
             print(curtimestamp, "Name:", i["GroupName"], "GroupNum", i["GroupNum"])
-
 
 if __name__ == '__main__':
     print("Starting up irspeccamswitcher...")
@@ -148,6 +145,8 @@ if __name__ == '__main__':
 
         # infinite loop
         read_cameras = 0
+        carNum = -1
+        camswitch_epoch = 1
         while True:
             # check if we are connected to iracing
             check_iracing()
@@ -158,18 +157,17 @@ if __name__ == '__main__':
                 curtimestamp = now.strftime("%y-%m-%d %H:%M:%S")
 
                 if read_cameras == 0:
-                    cameras()
-
+                    #cameras()
                     read_cameras = 1
-                idx=-1
+
                 # print some session infos
                 if ir["SessionNum"] != sessionNum:
                     sessionNum = ir["SessionNum"]
                     sessionName = ir["SessionInfo"]["Sessions"][sessionNum]["SessionName"]
                     print(curtimestamp, "Current Session is ", sessionName)
-                    idx=findDriver(druid)
+                    carNum=findDriver(druid)
 
-                loop()
+                camswitch_epoch = loop(carNum, camswitch_epoch)
             # sleep for 1 second
             # maximum you can use is 1/60
             # cause iracing updates data with 60 fps
